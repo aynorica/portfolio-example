c3c3544617af57d64f39acd025301d6aa12eba99
aynorica <amirdeilamizadeh1996@gmail.com>
2020-05-04 20:52:29 -0700

    Updates

---
+++ precache-manifest.ed586fddb0be9f06a89d61f39c36d280.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50da43f10d8ec8e82efd410259e74f6f",
+    "url": "/portfolio-exmps/index.html"
+  },
+  {
+    "revision": "79161a7421b88015076c",
+    "url": "/portfolio-exmps/static/css/2.f2ad0615.chunk.css"
+  },
+  {
+    "revision": "f63d1a0fef6efa3a427c",
+    "url": "/portfolio-exmps/static/css/3.0481794f.chunk.css"
+  },
+  {
+    "revision": "cfff06f6d584eecd8438",
+    "url": "/portfolio-exmps/static/css/main.9ddad8a9.chunk.css"
+  },
+  {
+    "revision": "79161a7421b88015076c",
+    "url": "/portfolio-exmps/static/js/2.edc1f256.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-exmps/static/js/2.edc1f256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63d1a0fef6efa3a427c",
+    "url": "/portfolio-exmps/static/js/3.bfae1baa.chunk.js"
+  },
+  {
+    "revision": "cfff06f6d584eecd8438",
+    "url": "/portfolio-exmps/static/js/main.7e52e186.chunk.js"
+  },
+  {
+    "revision": "2cc1c57cae4f9744629c",
+    "url": "/portfolio-exmps/static/js/runtime-main.1d2804a9.js"
+  },
+  {
+    "revision": "1af35008db31fe6fc456f240f08e689c",
+    "url": "/portfolio-exmps/static/media/Article-pic.1af35008.jpeg"
+  },
+  {
+    "revision": "3729e96723db3bcb04c53fca2caef696",
+    "url": "/portfolio-exmps/static/media/HomePic.3729e967.jpg"
+  },
+  {
+    "revision": "a59beb510aa3a2f42e5e1a2665b55217",
+    "url": "/portfolio-exmps/static/media/LogoFont.a59beb51.ttf"
+  },
+  {
+    "revision": "572567eca021d3e795de0bae1a37f408",
+    "url": "/portfolio-exmps/static/media/arrow.572567ec.svg"
+  },
+  {
+    "revision": "20b4b82aa9c8934fb1410a5e533a3920",
+    "url": "/portfolio-exmps/static/media/arrows.20b4b82a.svg"
+  },
+  {
+    "revision": "78e4334f9e0dd75d96309b482004c446",
+    "url": "/portfolio-exmps/static/media/gmail.78e4334f.svg"
+  },
+  {
+    "revision": "8d7d8fa0b9ddb8ec258ad6b8b9b6d254",
+    "url": "/portfolio-exmps/static/media/headFont.8d7d8fa0.ttf"
+  },
+  {
+    "revision": "c0817de39aba76db581d528b294fac15",
+    "url": "/portfolio-exmps/static/media/instagram.c0817de3.svg"
+  },
+  {
+    "revision": "52e3a1f3e90b465e53f879bebf65d8d7",
+    "url": "/portfolio-exmps/static/media/main_font.52e3a1f3.ttf"
+  },
+  {
+    "revision": "6d015af0cd9ff35b3ea43d1d2603a4b3",
+    "url": "/portfolio-exmps/static/media/soundcloud.6d015af0.svg"
+  },
+  {
+    "revision": "67839acefdd2c107021956a295b35b5a",
+    "url": "/portfolio-exmps/static/media/telegram.67839ace.svg"
+  },
+  {
+    "revision": "316cfa6d38658b02aacdf77c1659f1a5",
+    "url": "/portfolio-exmps/static/media/whatsapp.316cfa6d.svg"
+  }
+]);
