bfc704646017068002086b7b2ef9d59394ba146b
aynorica <amirdeilamizadeh1996@gmail.com>
2020-05-04 20:53:16 -0700

    Updates

---
+++ precache-manifest.5c313dc914a9830ea9b7f0093a4d5304.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5770a94470b35541d84d2691e04caeb9",
+    "url": "/portfolio-example/index.html"
+  },
+  {
+    "revision": "79161a7421b88015076c",
+    "url": "/portfolio-example/static/css/2.f2ad0615.chunk.css"
+  },
+  {
+    "revision": "f63d1a0fef6efa3a427c",
+    "url": "/portfolio-example/static/css/3.0481794f.chunk.css"
+  },
+  {
+    "revision": "4dc66ce4dcb6403d23c3",
+    "url": "/portfolio-example/static/css/main.558ac592.chunk.css"
+  },
+  {
+    "revision": "79161a7421b88015076c",
+    "url": "/portfolio-example/static/js/2.edc1f256.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-example/static/js/2.edc1f256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63d1a0fef6efa3a427c",
+    "url": "/portfolio-example/static/js/3.bfae1baa.chunk.js"
+  },
+  {
+    "revision": "4dc66ce4dcb6403d23c3",
+    "url": "/portfolio-example/static/js/main.985d818c.chunk.js"
+  },
+  {
+    "revision": "367e3df726230e5e7996",
+    "url": "/portfolio-example/static/js/runtime-main.12c58b88.js"
+  },
+  {
+    "revision": "1af35008db31fe6fc456f240f08e689c",
+    "url": "/portfolio-example/static/media/Article-pic.1af35008.jpeg"
+  },
+  {
+    "revision": "3729e96723db3bcb04c53fca2caef696",
+    "url": "/portfolio-example/static/media/HomePic.3729e967.jpg"
+  },
+  {
+    "revision": "a59beb510aa3a2f42e5e1a2665b55217",
+    "url": "/portfolio-example/static/media/LogoFont.a59beb51.ttf"
+  },
+  {
+    "revision": "572567eca021d3e795de0bae1a37f408",
+    "url": "/portfolio-example/static/media/arrow.572567ec.svg"
+  },
+  {
+    "revision": "20b4b82aa9c8934fb1410a5e533a3920",
+    "url": "/portfolio-example/static/media/arrows.20b4b82a.svg"
+  },
+  {
+    "revision": "78e4334f9e0dd75d96309b482004c446",
+    "url": "/portfolio-example/static/media/gmail.78e4334f.svg"
+  },
+  {
+    "revision": "8d7d8fa0b9ddb8ec258ad6b8b9b6d254",
+    "url": "/portfolio-example/static/media/headFont.8d7d8fa0.ttf"
+  },
+  {
+    "revision": "c0817de39aba76db581d528b294fac15",
+    "url": "/portfolio-example/static/media/instagram.c0817de3.svg"
+  },
+  {
+    "revision": "52e3a1f3e90b465e53f879bebf65d8d7",
+    "url": "/portfolio-example/static/media/main_font.52e3a1f3.ttf"
+  },
+  {
+    "revision": "6d015af0cd9ff35b3ea43d1d2603a4b3",
+    "url": "/portfolio-example/static/media/soundcloud.6d015af0.svg"
+  },
+  {
+    "revision": "67839acefdd2c107021956a295b35b5a",
+    "url": "/portfolio-example/static/media/telegram.67839ace.svg"
+  },
+  {
+    "revision": "316cfa6d38658b02aacdf77c1659f1a5",
+    "url": "/portfolio-example/static/media/whatsapp.316cfa6d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-exmps/precache-manifest.ed586fddb0be9f06a89d61f39c36d280.js"
+  "/portfolio-example/precache-manifest.5c313dc914a9830ea9b7f0093a4d5304.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-exmps/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
